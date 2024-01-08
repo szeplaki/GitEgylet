bef4b24d3681170243216b9fa5ea78d6ecdf79c5
szeplaki <eva.szeplaki@gmail.com>
2024-01-08 11:52:48 +0100

    add for loop to print numbers from 5 to 10

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,5 +3,8 @@ package org.example;
 public class App {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        for (int i = 5 ; i < 10; i++) {
+            System.out.println(i);
+        }
     }
 }
