0a49986833059147a500d3a7efbaf1a69f2cf91d
szeplaki <eva.szeplaki@gmail.com>
2024-01-08 11:58:05 +0100

    print hello saturnus

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -2,6 +2,7 @@ package org.example;
 
 public class App {
     public static void main( String[] args ) {
-        System.out.println( "Hello World!" );
+        System.out.println( "Hello Earth!" );
+        System.out.println("Hello Saturnus");
     }
 }
