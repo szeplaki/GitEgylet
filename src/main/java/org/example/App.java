38df3081724ac92571b1c92b4a1f5769c546d889
szeplaki <eva.szeplaki@gmail.com>
2024-01-08 11:54:20 +0100

    print hello mars

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -3,6 +3,7 @@ package org.example;
 public class App {
     public static void main( String[] args ) {
         System.out.println( "Hello World!" );
+        System.out.println( "Hello Mars");
         for (int i = 5 ; i < 10; i++) {
             System.out.println(i);
         }
