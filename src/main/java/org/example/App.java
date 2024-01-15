709a8309328aa050294a56254097c65fd48793d6
szeplaki <eva.szeplaki@gmail.com>
2024-01-15 11:27:57 +0100

    print hello master

--- src/main/java/org/example/App.java
+++ src/main/java/org/example/App.java
@@ -4,5 +4,6 @@ public class App {
     public static void main( String[] args ) {
         System.out.println( "Hello Earth!" );
         System.out.println("Hello Saturnus");
+        System.out.println("Hello master");
     }
 }
